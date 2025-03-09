b6d482a97b4362c0b357580ac733fcea23ef8123
Mohammed Nasser <81932030+MohammedNAbdelsamea@users.noreply.github.com>
2025-03-09 21:44:11 +0200

    Update loading_overlay.dart

--- lib/features/auth/presentation/widgets/loading_overlay.dart
+++ lib/features/auth/presentation/widgets/loading_overlay.dart
@@ -1,7 +1,8 @@
 import 'package:eventure/core/utils/theme/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:eventure/core/utils/theme/theme_cubit/theme_cubit.dart';
 
 class LoadingOverlay extends StatelessWidget {
   final String message;
@@ -13,21 +14,34 @@ class LoadingOverlay extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.black45,
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-             CircularProgressIndicator(color: kButton,),
-             SizedBox(height: 16.h),
-            Text(
-              message,
-              style:  TextStyle(color: Colors.white, fontSize: 16.sp),
+    return BlocBuilder<ThemeCubit, bool>(
+      builder: (context, isDarkMode) {
+        return Container(
+          color: isDarkMode
+              ? Colors.black.withValues(alpha: 0.5)
+              : kPreIcon.withValues(alpha: 0.5),
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircularProgressIndicator(
+                  color: isDarkMode ? Colors.white : kButton,
+                ),
+                SizedBox(height: 16.h),
+                Text(
+                  message,
+                  style: TextStyle(
+                    color: isDarkMode ? Colors.white : kMainLight,
+                    fontSize: 16.sp,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
